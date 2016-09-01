4d768ca026940a0e649269df3d1a154fbf159c43
Oliver Freyermuth <o.freyermuth@googlemail.com>
2016-09-01 19:47:35 +0200

    Solutions for Concepts, introspect not working yet

--- Concepts/emulate.cc
+++ Concepts/emulate.cc
@@ -5,12 +5,17 @@
 #include <vector>
 #include <type_traits>
 
+template <typename Iter>
+struct is_random_access_iterator : std::is_base_of<
+	std::random_access_iterator_tag,
+	typename std::iterator_traits<Iter>::iterator_category
+	> {};
 
 template <typename Iter, typename Cmp>
 void checkedSort(Iter first, Iter last, Cmp cmp) {
+	static_assert(is_random_access_iterator<Iter>(), "Need random access iterator for sorting!");
+
   using ItCatTag = typename std::iterator_traits<Iter>::iterator_category;
-
-
 
   std::sort(first, last, cmp);
 }
